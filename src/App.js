4cc9cf4508b38e1d10a467ed38d5a40db75f0fe8
Saber_Ben_Hamouda <benhamoudasabeur@gmail.com>
2024-06-13 13:08:13 +0200

    🔧 add student dynamically

--- src/App.js
+++ src/App.js
@@ -5,17 +5,15 @@ import Student from './components/Student.js';
 
 function App() {
   const [students,setStudents] = useState([
-    {student : "Saber Ben Hamouda" , astronomy : "A+ 98.4%", introductoryLogic : "A 94%", intermediateLogic : "A+ 99.3%"},
-    {student : "Ahmed Ben Hamouda" , astronomy : "B 88.4%", introductoryLogic : "A 92%", intermediateLogic : "A 91.3%"},
-    {student : "Asma Ben Hamouda" , astronomy : "C 78.4%", introductoryLogic : "A 80%", intermediateLogic : "A+ 95.3%"},
-    {student : "ALi Ben Hamouda" , astronomy : "A+ 98.4%", introductoryLogic : "A 85%", intermediateLogic : "D 60.3%"}
+    {student : "Saber Ben Hamouda" , astronomy : "A+ 98.4%", introductoryLogic : "A 94%", intermediateLogic : "A+ 99.3%"}
   ]);
-  const [ArrNewStudent,setArrNewStudent] = useState([]);
+  
   const [newSubject,setNewSubject] =useState('');
+  const [newStudent,setNewStudent] = useState('');
   
   const handleInputStudent = (e) =>{
     const newStudent = e.target.value;
-    setArrNewStudent(newStudent.split(" "))
+    setNewStudent(newStudent)
   }
   const handleInputSubject = (e) =>{
     const newSubject = e.target.value;
@@ -30,13 +28,18 @@ function App() {
   
   
   const handleAddStudent = () =>{
+    const student = students[0] ;
     setStudents(
-      (oldStudents) => 
-        {const newStudents =oldStudents.concat([
-          {student :ArrNewStudent[0] ,astronomy : ArrNewStudent[1], introductoryLogic :  ArrNewStudent[2] ,intermediateLogic :ArrNewStudent[3] }
-        ]);
-        setMappedStudents(newStudents.map(mapper));
-        return newStudents;
+      (oldStudents) =>
+        {
+          let newStudentObject ={}
+          Object.keys(student).forEach(key => newStudentObject[key]='')
+
+          newStudentObject = {...newStudentObject,student : newStudent}
+
+          const newStudents =oldStudents.concat([newStudentObject]);
+          setMappedStudents(newStudents.map(mapper));
+          return newStudents;
       }
     );
   }
@@ -50,11 +53,9 @@ function App() {
         return newStudents;
       }
     );
-    console.log(students);
   }
-  const mappedSubjects= Object.keys(students[0]).map((key)=>(<td>{key}</td>))
-  console.log(Object.keys(students));
 
+  const mappedSubjects= Object.keys(students[0]).map((key)=>(<td>{key}</td>))
   return (
     <div>
       <div class="container">
